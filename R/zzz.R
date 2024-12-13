fcfc54b8c2aec6c0314ac4ae448e95bb3f1281c6
John Benedict Palo <jrpalo@doh.gov.ph>
2024-12-13 11:22:46 +0800

    fixed onLoad handler for non-dqt of nhsss workloads

--- R/zzz.R
+++ R/zzz.R
@@ -6,41 +6,41 @@
 }
 
 .onLoad <- function(libname, pkgname) {
-   if (Sys.getenv("DB_USER") == "") {
+   if (Sys.getenv("DB_USER") != "") {
+
+      if (!("oh-live" %in% ls(envir = .GlobalEnv))) {
+         .GlobalEnv$`oh-live` <- pool::dbPool(
+            RMariaDB::MariaDB(),
+            user     = Sys.getenv("DB_USER"),
+            password = Sys.getenv("DB_PASS"),
+            host     = Sys.getenv("DB_HOST"),
+            port     = Sys.getenv("DB_PORT"),
+            timeout  = -1
+         )
+      }
+
+      if (!("oh-lw" %in% ls(envir = .GlobalEnv))) {
+         .GlobalEnv$`oh-lw` <- pool::dbPool(
+            RMariaDB::MariaDB(),
+            user     = Sys.getenv("LW_USER"),
+            password = Sys.getenv("LW_PASS"),
+            host     = Sys.getenv("LW_HOST"),
+            port     = Sys.getenv("LW_PORT"),
+            timeout  = -1
+         )
+      }
+
+      reg.finalizer(
+         e      = .GlobalEnv,
+         f      = function() {
+            pool::poolClose(.GlobalEnv$`oh-live`)
+            pool::poolClose(.GlobalEnv$`oh-lw`)
+         },
+         onexit = TRUE
+      )
+
       invisible(TRUE)
    }
-
-
-   if (!("oh-live" %in% ls(envir = .GlobalEnv))) {
-      .GlobalEnv$`oh-live` <- pool::dbPool(
-         RMariaDB::MariaDB(),
-         user     = Sys.getenv("DB_USER"),
-         password = Sys.getenv("DB_PASS"),
-         host     = Sys.getenv("DB_HOST"),
-         port     = Sys.getenv("DB_PORT"),
-         timeout  = -1
-      )
-   }
-
-   if (!("oh-lw" %in% ls(envir = .GlobalEnv))) {
-      .GlobalEnv$`oh-lw` <- pool::dbPool(
-         RMariaDB::MariaDB(),
-         user     = Sys.getenv("LW_USER"),
-         password = Sys.getenv("LW_PASS"),
-         host     = Sys.getenv("LW_HOST"),
-         port     = Sys.getenv("LW_PORT"),
-         timeout  = -1
-      )
-   }
-
-   reg.finalizer(
-      e      = .GlobalEnv,
-      f      = function() {
-         pool::poolClose(.GlobalEnv$`oh-live`)
-         pool::poolClose(.GlobalEnv$`oh-lw`)
-      },
-      onexit = TRUE
-   )
 
    invisible(TRUE)
 }
